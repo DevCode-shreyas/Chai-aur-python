428aec3f63c9b1d54d0a82e102e6b393f8c85258
shreyas bole <shreyasbole4@gmail.com>
2024-05-16 22:42:08 +0530

    refactor: Improve grade calculation logic in 02_conditions/3_solution.py

---
+++ 02_conditions/3_solution.py
@@ -0,0 +1,18 @@
+score = 155
+
+if score >= 101:
+    print("Invalid score")
+    exit()
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"Your grade is {grade}")
