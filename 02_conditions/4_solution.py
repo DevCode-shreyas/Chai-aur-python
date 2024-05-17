560d9c17c8387eb039f1885de5e77cab57462d1e
shreyas bole <shreyasbole4@gmail.com>
2024-05-17 22:50:06 +0530

    Refactor fruit ripeness logic in 02_conditions/4_solution.py

---
+++ 02_conditions/4_solution.py
@@ -0,0 +1,13 @@
+fruit = "banana"
+color = "yellow"
+
+
+if fruit =="banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("Overripe")
+else:
+    print("I don't know what to do with this fruit")
