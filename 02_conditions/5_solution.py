ac38c19a7094bb0fea7bffe6c7b731a0ecac0487
shreyas bole <shreyasbole4@gmail.com>
2024-05-18 20:23:41 +0530

    Refactor weather activity logic in 02_conditions/5_solution.py

---
+++ 02_conditions/5_solution.py
@@ -0,0 +1,12 @@
+weather = "sunny"
+
+if weather == "sunny":
+    activity = "go for a walk"
+elif weather == "rainy":
+    activity = "stay inside"
+elif weather == "snowy":
+    activity = "build a snowman"
+else:
+    activity = "do something else"
+
+print(activity)
