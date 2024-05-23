b50ea2806ff52b0ad6ae024159b3398ca4d56760
shreyas bole <shreyasbole4@gmail.com>
2024-05-23 22:19:11 +0530

    refactor: Add code for pet food selection based on pet type and age

---
+++ 02_conditions/10_solution.py
@@ -0,0 +1,17 @@
+pet = input("Enter a pet: ")
+pet_age = int(input("Enter the age of the pet: "))
+
+if pet == "Dog":
+    if pet_age <2:
+        print("Puppy Food")
+    else:
+        print("Adult Dog Food")
+elif pet == "Cat":
+    if pet_age >5:
+        print("Senior Cat Food")
+    else:
+        print("Baby Cat Food")
+else:
+    print("Unknown Pet")
+
+    
