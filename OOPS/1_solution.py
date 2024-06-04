38cc867e60257b821f3da0f49297b924c995dcfa
shreyas bole <shreyasbole4@gmail.com>
2024-06-04 21:50:31 +0530

    refactor: Improve Car class inheritance hierarchy and remove unused code

--- OOPS/1_solution.py
+++ OOPS/1_solution.py
@@ -1,88 +1,88 @@
-# class Car:
-#     total_car = 0
+class Car:
+    total_car = 0
 
-#     def __init__(self,brand,model):
-#        self.__brand = brand
-#        self.__model = model
-#        Car.total_car += 1
+    def __init__(self,brand,model):
+       self.__brand = brand
+       self.__model = model
+       Car.total_car += 1
 
-#     def get_brand(self):
-#         return self.__brand
+    def get_brand(self):
+        return self.__brand
 
-#     def full_name(self):
-#         return f"{self.__brand} {self.__model}"
+    def full_name(self):
+        return f"{self.__brand} {self.__model}"
     
-#     def fuel_type(self):
-#         return "Petrol or Diesel"
+    def fuel_type(self):
+        return "Petrol or Diesel"
     
-#     @staticmethod
-#     def general_desc():
-#         return "cars are good for transportation"
+    @staticmethod
+    def general_desc():
+        return "cars are good for transportation"
     
-#     @property
-#     def model(self):
-#         return self.__model
+    @property
+    def model(self):
+        return self.__model
     
 
 
 
-# class ElectricCar(Car):
-#     def __init__(self,brand,model,battery_size):
-#         super().__init__(brand,model)
-#         self.battery_size = battery_size
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size = battery_size
 
-#     def fuel_type(self):
-#         return "Electric charge"
-
-
-# my_tesla = ElectricCar("Tesla","Model S","100")
-
-# print(isinstance(my_tesla,Car))
+    def fuel_type(self):
+        return "Electric charge"
 
 
+my_tesla = ElectricCar("Tesla","Model S","100")
 
-# print(my_tesla.battery_size)
-# print(my_tesla.model)
-# print(my_tesla.full_name())
-# print(my_tesla.__brand)
-# print(my_tesla.fuel_type())
-
-# my_car = Car("Tata","Safari")
-# my_car.model = "City"
-# print(my_car.model) 
-
-# print(my_car.general_desc())
-# print(Car.general_desc())
-# print(safari.fuel_type())
-# print(Car.total_car)
+print(isinstance(my_tesla,Car))
 
 
 
-# my_car = Car("Toyota","Corolla")
+print(my_tesla.battery_size)
+print(my_tesla.model)
+print(my_tesla.full_name())
+print(my_tesla.__brand)
+print(my_tesla.fuel_type())
 
-# print(my_car.brand)
-# print(my_car.model)
-# print(my_car.full_name())
+my_car = Car("Tata","Safari")
+my_car.model = "City"
+print(my_car.model) 
 
-
-# my_new_car = Car("Honda","Civic")
-# print(my_new_car.model)
-
-
-
-# class Battery:
-#     def battery_info(self):
-#         return "this is battery"
-
-# class Engine:
-#     def engine_info(self):
-#         return "this is engine"
+print(my_car.general_desc())
+print(Car.general_desc())
+print(safari.fuel_type())
+print(Car.total_car)
 
 
-# class ElectricCarTwo(Battery,Engine,Car):
-#     pass
+
+my_car = Car("Toyota","Corolla")
+
+print(my_car.brand)
+print(my_car.model)
+print(my_car.full_name())
 
 
-# my_new_tesla = ElectricCarTwo("Tesla","Model S")
-# print(my_new_tesla.battery_info()) 
-# print(my_new_tesla.engine_info())
+my_new_car = Car("Honda","Civic")
+print(my_new_car.model)
+
+
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+
+class Engine:
+    def engine_info(self):
+        return "this is engine"
+
+
+class ElectricCarTwo(Battery,Engine,Car):
+    pass
+
+
+my_new_tesla = ElectricCarTwo("Tesla","Model S")
+print(my_new_tesla.battery_info()) 
+print(my_new_tesla.engine_info())
