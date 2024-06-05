2706dc953f4dad21b2bbabf2bca49639be77fb99
shreyas bole <shreyasbole4@gmail.com>
2024-06-05 21:43:03 +0530

    refactor: Remove commented code and improve Car class inheritance hierarchy

--- OOPS/1_solution.py
+++ OOPS/1_solution.py
@@ -1,27 +1,27 @@
-class Car:
-    total_car = 0
+# class Car:
+#     total_car = 0
 
-    def __init__(self,brand,model):
-       self.__brand = brand
-       self.__model = model
-       Car.total_car += 1
+#     def __init__(self,brand,model):
+#        self.__brand = brand
+#        self.__model = model
+#        Car.total_car += 1
 
-    def get_brand(self):
-        return self.__brand
+#     def get_brand(self):
+#         return self.__brand
 
-    def full_name(self):
-        return f"{self.__brand} {self.__model}"
+#     def full_name(self):
+#         return f"{self.__brand} {self.__model}"
     
-    def fuel_type(self):
-        return "Petrol or Diesel"
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
     
-    @staticmethod
-    def general_desc():
-        return "cars are good for transportation"
+#     @staticmethod
+#     def general_desc():
+#         return "cars are good for transportation"
     
-    @property
-    def model(self):
-        return self.__model
+#     @property
+#     def model(self):
+#         return self.__model
     
 
 
